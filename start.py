7b2008a604ce006bc4d5b319a97a9bfedb74ceb5
YaRo <roman.yatsenko@hneu.net>
2023-01-18 19:27:41 +0200

    Add _check_events

--- start.py
+++ start.py
@@ -23,9 +23,7 @@ class AlienInvasion:
         """Запуск основного цикла игры"""
         while True:
             # отслеживание событий клавиатуры и мыши
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
+            self._check_events()
 
             # При каждом проходе цикла перерисовывается экран
             self.screen.fill(self.settings.bg_color)
@@ -33,6 +31,12 @@ class AlienInvasion:
 
             # Отображение последнего прорисованного экрана
             pygame.display.flip()
+
+    def _check_events(self):
+        """Обрабатывает нажатия клавиш и события мыши"""
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
 
 if __name__ == '__main__':
     # Создание экземпляра и запуск игры
