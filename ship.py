cd35a5368e7f1d944aeb927657f8bb0d4b14d0f5
YaRo <roman.yatsenko@hneu.net>
2023-01-18 20:20:39 +0200

    Add ship speed

--- ship.py
+++ ship.py
@@ -5,6 +5,7 @@ class Ship():
 
     def __init__(self, ai_game):
         """Инициализирует корабль и задает его начальную позицию"""
+        self.settings = ai_game.settings
         self.screen = ai_game.screen
         self.screen_rect = ai_game.screen.get_rect()
 
@@ -14,16 +15,23 @@ class Ship():
         # Каждый новый корабль появляется у нижнего края экрана
         self.rect.midbottom = self.screen_rect.midbottom
 
+        # Сохранение вещественной координаты центра корабля
+        self.x = float(self.rect.x)
+
         # Флаг перемещения
         self.moving_right = False
         self.moving_left = False
 
     def update(self):
         """Обновляет позицию корабля с учетом флага"""
+        # Обновляется атрибут x, а не rect
         if self.moving_right:
-            self.rect.x += 1
+            self.x += self.settings.ship_speed
         if self.moving_left:
-            self.rect.x -= 1
+            self.x -= self.settings.ship_speed
+
+        # Обновление атрибута rect на основании self.x
+        self.rect.x = self.x
 
     def blitme(self):
         """Рисует корабль в текущей позиции"""
