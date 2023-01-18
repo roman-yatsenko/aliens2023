1d6d050045c647bf5084d8f95da86d6af686423c
YaRo <roman.yatsenko@hneu.net>
2023-01-18 19:31:35 +0200

    Add _update_screen

--- start.py
+++ start.py
@@ -22,21 +22,22 @@ class AlienInvasion:
     def run_game(self):
         """Запуск основного цикла игры"""
         while True:
-            # отслеживание событий клавиатуры и мыши
             self._check_events()
-
-            # При каждом проходе цикла перерисовывается экран
-            self.screen.fill(self.settings.bg_color)
-            self.ship.blitme()
-
-            # Отображение последнего прорисованного экрана
-            pygame.display.flip()
+            self._update_screen()
 
     def _check_events(self):
         """Обрабатывает нажатия клавиш и события мыши"""
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+
+    def _update_screen(self):
+        """Обновляет изображения на экране и отображает новый экран"""
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
+
+        # Отображение последнего прорисованного экрана
+        pygame.display.flip()
 
 if __name__ == '__main__':
     # Создание экземпляра и запуск игры
