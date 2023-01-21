31df486111e7b4ca276e0a1285c8b740ab41e57e
YaRo <roman.yatsenko@hneu.net>
2023-01-21 14:26:22 +0200

    Add bullet limit

--- start.py
+++ start.py
@@ -67,8 +67,9 @@ class AlienInvasion:
 
     def _fire_bullet(self):
         """Создание нового снаряда и включение его в группу bullets"""
-        new_bullet = Bullet(self)
-        self.bullets.add(new_bullet)
+        if len(self.bullets) < self.settings.bullets_allowed:
+            new_bullet = Bullet(self)
+            self.bullets.add(new_bullet)
 
     def _update_screen(self):
         """Обновляет изображения на экране и отображает новый экран"""
