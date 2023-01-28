d6c955d24274766be28adcce013f92220b60dc08
YaRo <roman.yatsenko@hneu.net>
2023-01-28 14:22:20 +0200

    Add alien row

--- start.py
+++ start.py
@@ -29,9 +29,20 @@ class AlienInvasion:
 
     def _create_fleet(self):
         """Створення флота прибульців"""
-        # Створення прибульця
+        # Створення прибульця і обрахунок кількості прибульців в ряду
+        # Інтервал між сусідніми прибульцями дорівнює ширині прибульця
         alien = Alien(self)
-        self.aliens.add(alien)
+        alien_width = alien.rect.width
+        available_space_x = self.settings.screen_width - (2 * alien_width)
+        number_aliens_x = available_space_x // (2 * alien_width)
+
+        # Створення першого ряду прибульців
+        for alien_number in range(number_aliens_x):
+            # Створення прибульця та розміщення його в ряду
+            alien = Alien(self)
+            alien.x = alien_width + 2 * alien_width * alien_number
+            alien.rect.x = alien.x
+            self.aliens.add(alien)
 
     def run_game(self):
         """Запуск основного цикла игры"""
