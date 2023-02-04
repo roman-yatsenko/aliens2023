3358ff88a53564ed42f9afe07645e69fff031f68
YaRo <roman.yatsenko@hneu.net>
2023-02-04 13:57:19 +0200

    Refactor update_bullets

--- start.py
+++ start.py
@@ -120,6 +120,9 @@ class AlienInvasion:
             if bullet.rect.bottom <= 0:
                 self.bullets.remove(bullet)
         
+        self._check_bullet_alien_collisions()
+
+    def _check_bullet_alien_collisions(self):
         # Перевірка потраплянь у прибульців
         collisions = pygame.sprite.groupcollide(self.bullets, self.aliens, True, True)
         
