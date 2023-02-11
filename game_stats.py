1c96bef2f937f65aeae1c49e77575eeac7558d91
YaRo <roman.yatsenko@hneu.net>
2023-02-11 13:53:47 +0200

    Add Button class

--- game_stats.py
+++ game_stats.py
@@ -6,8 +6,8 @@ class GameStats():
         self.settings = ai_game.settings
         self.reset_stats()
 
-        # Гра запускається в активному стані
-        self.game_active = True
+        # Гра запускається в неактивному стані
+        self.game_active = False
 
     def reset_stats(self):
         """Ініціалізує статистику, що змінюється під час гри"""
